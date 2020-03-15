7c29f74eecd3f544732cbcf9086108187b30acf1
jiajinNervermore <58249513+jiajinNervermore@users.noreply.github.com>
2020-03-15 13:56:07 +0800

    v-0315-1

--- routes/details.js
+++ routes/details.js
@@ -3,3 +3,17 @@ const express = require("express")
 var router = express.Router();
 //引入封装好了的连接池语句
 var query = require("./query.js")
+let pool = require("../pool.js")
+//引入封装好了的连接池语句
+// var query = require("./query.js")
+server.get("/details",(req,res)=>{
+  var sql = "SELECT title,price1,details_pic,imags FROM xhs_product"
+  pool.query(sql,(err,result)=>{
+    if(err)throw err;
+    if(result.length==0){
+      res.send({code:-1,msg:"没有找到相关信息"});
+    }else{
+      res.send(result)
+    }
+  })
+})
